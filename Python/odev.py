22b7139ede699c4f0f3ddc0e4b980bcdcec87186
Cüneyt Anıl Sağır <canilsagir@gmail.com>
2023-04-08 22:34:40 +0300

    Create odev.py

---
+++ Python/odev.py
@@ -0,0 +1,15 @@
+#kodlama.io sitesinde current_user/profile sekmesinde Full Name içerisinde tutulan veriler String veri tipidir.
+
+#kodlama.io sitesine log in tarafıda Giriş Yap blogunda email ve password dogruysa log in olabilirsin yanlışsa log in olunamaz.
+
+
+password =123
+
+userName ="Anil"
+giris ="Kullanici Dogru"
+if userName == "Anil":
+    print("Kullanici Adı Dogru") 
+    if password == 123:
+        print("Sifre Dogru. Giriş Yapıldı")
+else :     
+    print("Sifre Yanlis")   
